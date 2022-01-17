c892c28c241a8cf8b1bf3afcb09cebfcfa7f8f66
Ontorok <nasir.amath07@gmail.com>
2022-01-17 15:16:33 +0600

    progress bar add

--- src/components/ProgessBar.jsx
+++ src/components/ProgessBar.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import classes from "../styles/ProgessBar.module.css";
 
 const ProgessBar = () => {
-  return <div>Progressbar</div>;
+  return (
+    <div className={classes.progressBar}>
+      <div className={classes.backButton}>
+        <span className="material-icons-outlined"> arrow_back </span>
+      </div>
+      <div className={classes.rangeArea}>
+        <div className={classes.tooltip}>24% Complete!</div>
+        <div className={classes.rangeBody}>
+          <div className={classes.progress} style={{ width: "20%" }}></div>
+        </div>
+      </div>
+
+      <a href="result.html">
+        <div className={classes.nextButton}>
+          <span className="material-icons-outlined"> arrow_forward </span>
+        </div>
+      </a>
+    </div>
+  );
 };
 
 export default ProgessBar;
