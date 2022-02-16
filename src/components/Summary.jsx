d505da1587dfe922d5ae4a9f891a80f15de33c25
Ontorok <nasir.amath07@gmail.com>
2022-02-16 18:01:10 +0600

    fetch api add

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useMemo } from "react";
 import successImg from "../assets/images/success.png";
+import useFetch from "../hooks/useFetch";
 import classes from "../styles/Summary.module.css";
 
 const Summary = ({ score, noq }) => {
+  const getKeyword = useMemo(() => {
+    if ((score / (noq * 5)) * 100 < 50) {
+      return "failed";
+    } else if ((score / (noq * 5)) * 100 < 75) {
+      return "good";
+    } else if ((score / (noq * 5)) * 100 < 100) {
+      return "very good";
+    } else {
+      return "excellent";
+    }
+  }, [score, noq]);
+
+  const { loading, error, result } = useFetch(
+    `https://api.pexels.com/v1/search?query=${getKeyword}&per_page=1`,
+    "GET",
+    { Authorization: process.env.REACT_APP_PEXELS_API_KEY }
+  );
+  console.log(result);
+
+  const image = result ? result?.photos[0].src.medium : successImg;
+
   return (
     <div className={classes.summary}>
       <div className={classes.point}>
@@ -11,9 +33,14 @@ const Summary = ({ score, noq }) => {
           {score} out of {noq * 5}
         </p>
       </div>
-      <div className={classes.badge}>
-        <img src={successImg} alt="Success" />
-      </div>
+
+      {loading && <div className={classes.badge}>Loading your badge...</div>}
+      {error && <div className={classes.badge}>An error occured</div>}
+      {!loading && !error && (
+        <div className={classes.badge}>
+          <img src={image} alt="Success" />
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export default function useFetch(url, method, headers) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [result, setResult] = useState(null)
+
+  useEffect(() => {
+    async function requestFetch() {
+      try {
+        setLoading(true);
+        setError(false);
+        const res = await fetch(url, {
+          method: method || "GET",
+          headers: headers
+        })
+        const data = await res.json();
+        setLoading(false);
+        setResult(data);
+      } catch (err) {
+        setError(true);
+        setLoading(false);
+        console.log(err);
+      }
+    }
+    requestFetch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return {
+    loading, error, result
+  }
+
+}
